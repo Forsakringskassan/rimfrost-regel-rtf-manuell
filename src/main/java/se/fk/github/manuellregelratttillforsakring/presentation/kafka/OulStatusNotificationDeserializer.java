bef2b950e009830bd8cafabeb3bd59a88234f4f0
Nils Elveros <nils.elveros@tromb.com>
2025-12-04 09:27:17 +0100

    fix: spotless

--- src/main/java/se/fk/github/manuellregelratttillforsakring/presentation/kafka/OulStatusNotificationDeserializer.java
+++ src/main/java/se/fk/github/manuellregelratttillforsakring/presentation/kafka/OulStatusNotificationDeserializer.java
@@ -3,11 +3,12 @@ package se.fk.github.manuellregelratttillforsakring.presentation.kafka;
 import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
 import se.fk.rimfrost.OperativtUppgiftslagerStatusMessage;
 
-public class OulStatusNotificationDeserializer extends ObjectMapperDeserializer<OperativtUppgiftslagerStatusMessage> {
-    
-    public OulStatusNotificationDeserializer()
-    {
-        super(OperativtUppgiftslagerStatusMessage.class);
-    }
+public class OulStatusNotificationDeserializer extends ObjectMapperDeserializer<OperativtUppgiftslagerStatusMessage>
+{
+
+   public OulStatusNotificationDeserializer()
+   {
+      super(OperativtUppgiftslagerStatusMessage.class);
+   }
 
 }
