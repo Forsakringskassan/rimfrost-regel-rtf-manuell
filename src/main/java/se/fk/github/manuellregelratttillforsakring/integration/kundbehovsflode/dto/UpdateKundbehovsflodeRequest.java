c60996beba896af0fde2d45e8626373227ee3184
Nils Elveros <nils.elveros@tromb.com>
2025-12-04 15:50:06 +0100

    fix: allow uppgiftId null

--- src/main/java/se/fk/github/manuellregelratttillforsakring/integration/kundbehovsflode/dto/UpdateKundbehovsflodeRequest.java
+++ src/main/java/se/fk/github/manuellregelratttillforsakring/integration/kundbehovsflode/dto/UpdateKundbehovsflodeRequest.java
@@ -5,11 +5,14 @@ import java.util.UUID;
 
 import org.immutables.value.Value;
 
+import jakarta.annotation.Nullable;
+
 @Value.Immutable
 public interface UpdateKundbehovsflodeRequest
 {
    UUID kundbehovsflodeId();
 
+   @Nullable
    UUID uppgiftId();
 
    List<UpdateKundbehovsflodeErsattning> ersattningar();
